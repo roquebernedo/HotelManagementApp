8cb95f4b1521c7ea384f7443489722d28926b424
unknown <roquebernedo@gmail.com>
2025-11-12 16:59:24 -0500

    adding only login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,96 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Link } from 'react-router-dom'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleSubmit = () => {
+
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='login-in'>
+      {/* {isLoading ? (
+          <div className='loader'>
+            <CircleLoader color={'#157dc2'} loading={true} css={override} size={75} />
+          </div>
+        ):
+        <> */}
+          <form className='form-login' onSubmit={handleSubmit}>
+            <div className="login">
+              <section className='login-container'> 
+                <div className='login-title'>
+                  <h1 className='title'>
+                    <span className='title-main'>HOTEL</span>
+                    <span className='title-sub'>BLANCO</span>
+                  </h1>
+                  {/* <h2 className='subtitle'>Blanco</h2> */}
+                </div>
+                <div className='login-email'>
+                  {/* {errorsSignin.email?.type === "required" && ( */}
+                    {/* <p className="error-input">Ingresa tu email</p> */}
+                   {/* )} */}
+                  {/* {errorsSignin.email?.type !== "required" && ( */}
+                    {/* <label className='email lab'></label> */}
+                  {/* )} */}
+                  <p>Email o usuario</p>
+                  <input 
+                    type='text' 
+                    className='email-input'
+                    placeholder='Email'
+                    // onChange={(e) => setEmail(e.target.value)} 
+                    // {...registerSignin("email", {
+                    //   required: true,
+                    // })} 
+                    // required
+                    // onInvalid={(e) => e.target.setCustomValidity('Ingresa tu email')}
+                    // onInput={(e) => e.target.setCustomValidity('')}
+                  />
+                </div>
+                <div className='login-password'>
+                  {/* {errorsSignin.password?.type === "required" && ( */}
+                    {/* <p className="error-input">Ingresa tu contraseña</p> */}
+                  {/* )} */}
+                  {/* {errorsSignin.password?.type !== "required" && (
+                    <label className='password lab'></label>
+                  )} */}
+                  <p>Contraseña</p>
+                  <input 
+                    type='password' 
+                    className='password-input'
+                    placeholder='Contraseña'
+                    // onChange={(e) => setPassword(e.target.value)} 
+                    // {...registerSignin("password", {
+                    //   required: true,
+                    // })}
+                    // required
+                    // onInvalid={(e) => e.target.setCustomValidity('Ingresa tu contraseña')}
+                    // onInput={(e) => e.target.setCustomValidity('')} 
+                  />
+                  <Link className='password-save'>Recuperar contraseña</Link>
+                </div>
+                <div className='login-button'>
+                  <button className='log-in' type='submit'>Log in</button>
+                </div>
+                <div className='login-recover'>
+                  <div className='login-ask'>¿Necesitas una cuenta?</div>
+                  <Link className='login-create'>Crear cuenta</Link>
+                </div>
+              </section>
+            </div>
+            {/* <div className="google-container">
+              <span>O ingresa con tu cuenta de Google</span>
+              <span className="google-signin-container" id="signInDiv"></span>
+            </div> */}
+          </form>
+
+        {/* </> */}
+        {/* } */}
       </div>
-      <h1>Vite + React + Gaaa</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
