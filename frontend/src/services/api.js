555957a79c244a43b67891f9f077a67c3cf111a2
unknown <roquebernedo@gmail.com>
2026-02-10 15:46:23 -0500

    added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,8 +3,8 @@ import { deleteCookie, getCookie, setCookie } from "@/utils/cookies";
 
 // const BACK_URL = import.meta.env.VITE_BACK_URL
 //http://localhost:3001
-// https://hotelmanagementapp-5slj.onrender.com/
-const BACK_URL = "http://localhost:3001"
+// https://hotelmanagementapp-5slj.onrender.com
+const BACK_URL = "https://hotelmanagementapp-5slj.onrender.com"
 
 // const config = {
 //     //: TODO: set waiting for response time (failsafe for fatal errors on server and to avoid infinite loadings)
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
         })
     ],
     server: {
-        port: 3000,
+        port: process.env.PORT || 3000,
     },
     resolve: {
         alias: {
